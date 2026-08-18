de0e466481dc74a44c1de5b5a04f632822d9ccc9
stephanmunn <stephanmunn@hotmail.com>
2026-08-18 15:39:43 +0000

    added home.html to app01/urls.py

--- app01/urls.py
+++ app01/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('fn', include('fn.urls')),
     path('tr', include('tr.urls')),
+    path("", TemplateView.as_view(template_name="home.html"), name="home),
 ]
